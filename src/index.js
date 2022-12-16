c88fe470834026aaf1642e84ef0014232121f8d2
AyakaB <ayaka.bando22@gmail.com>
2022-12-16 12:42:55 -0800

    added Auth0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ProductsProvider } from './context/products_context';
@@ -8,13 +8,25 @@ import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_DOMAIN}
+    // domain="dev-t6j4gjeik4yiqxtc.us.auth0.com"
+    clientId={process.env.REACT_APP_CLIENTID}
+    // clientId="TcDtIhwZ2lxJ2qf4riKpm3DdrdPUlqR0"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );

9ca29bcce1adcf0aa771803db10650d06c7c6429
AyakaB <ayaka.bando22@gmail.com>
2022-12-18 13:07:09 -0800

    fixed some codes

--- functions/single-product.js
+++ functions/single-product.js
@@ -2,9 +2,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const Airtable = require('airtable-node');
-const airtable = new Airtable({ apiKey: process.env.REACT_APP_API_KEY })
-  .base(process.env.REACT_APP_BASE)
-  .table(process.env.REACT_APP_TABLE);
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base(process.env.AIRTABLE_BASE)
+  .table(process.env.AIRTABLE_TABLE);
 
 exports.handler = async (event, context, callback) => {
   const { id } = event.queryStringParameters;

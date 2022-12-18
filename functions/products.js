272384ca5028445d4f34e22337efcab9148fa505
AyakaB <ayaka.bando22@gmail.com>
2022-12-18 12:57:14 -0800

    changed contents

---
+++ functions/products.js
@@ -0,0 +1,56 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const Airtable = require('airtable-node');
+const airtable = new Airtable({ apiKey: process.env.REACT_APP_API_KEY })
+  .base(process.env.REACT_APP_BASE)
+  .table(process.env.REACT_APP_TABLE);
+
+exports.handler = async (event, context, callback) => {
+  try {
+    const response = await airtable.list({ maxRecords: 200 });
+
+    const products = response.records.map((product) => {
+      const { id, fields } = product;
+      const {
+        name,
+        featured,
+        price,
+        colors,
+        company,
+        description,
+        category,
+        shipping,
+        images,
+      } = fields;
+
+      const { url } = images[0];
+      return {
+        id,
+        featured,
+        name,
+        price,
+        colors,
+        company,
+        description,
+        category,
+        shipping,
+        image: url,
+      };
+    });
+    console.log('#####');
+    console.log(products);
+    console.log('#####');
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(products),
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: 'There was an error',
+    };
+  }
+};
